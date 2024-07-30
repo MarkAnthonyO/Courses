bc8a2d77c4c0e8871410ae33b78958143f7f09e4
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-07-30 09:49:20 -0600

    Update CourseCreatorView.kt

--- App/src/main/kotlin/views/CourseCreatorView.kt
+++ App/src/main/kotlin/views/CourseCreatorView.kt
@@ -3,11 +3,8 @@ package views
 import components.Course
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
-import javafx.fxml.Initializable
 import javafx.scene.control.TextField
 import window.Window
-import java.net.URL
-import java.util.*
 
 class CourseCreatorView {
 
