b2a21d473633ecf8df62ff98962ba813a3d6a412
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-07-30 08:33:30 -0600

    Update CoursesView.kt

--- App/src/main/kotlin/views/CoursesView.kt
+++ App/src/main/kotlin/views/CoursesView.kt
@@ -1,11 +1,38 @@
 package views
 
+import components.Course
+import connection.SQLiteConnection
+import getter.CourseGetter
 import javafx.event.ActionEvent
+import javafx.fxml.Initializable
 import window.Window
+import java.net.URL
+import java.util.*
 
-class CoursesView {
+class CoursesView : Initializable{
+
+    fun createCourse(event: ActionEvent) {
+        Window.getWindow().changeToView("course_creator_view")
+    }
+
     fun back(event: ActionEvent) {
-        var w = Window.getWindow()
+        val w = Window.getWindow()
         w.changeToView("students_view")
+    }
+
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+        SQLiteConnection.openConnection()
+        val courses : ArrayList<Course> = CourseGetter.getAll()
+
+        if(courses.isEmpty()) {
+            println("No existen cursos")
+            SQLiteConnection.closeConnection()
+            return
+        }
+
+        courses.forEach { course ->
+            println(course.name)
+        }
+        SQLiteConnection.closeConnection()
     }
 }
