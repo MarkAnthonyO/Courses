4927c7781a105f7f07b38df6434fac7cd37bbfa6
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-07-30 19:49:35 -0600

    Update CourseCreatorView.kt

--- App/src/main/kotlin/views/CourseCreatorView.kt
+++ App/src/main/kotlin/views/CourseCreatorView.kt
@@ -1,10 +1,14 @@
 package views
 
 import components.Course
+import components.Student
+import getter.TeacherGetter
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
+import javafx.scene.control.Alert
 import javafx.scene.control.TextField
 import window.Window
+import java.util.ArrayList
 
 class CourseCreatorView {
 
@@ -13,12 +17,24 @@ class CourseCreatorView {
 
     fun addCourse(event: ActionEvent) {
         val course = Course()
-        course.name = txtName.text
 
-        println(course.name)
+        if(txtName.text.equals("")){
+            showMessage()
+            return
+        }
+
+        course.name = txtName.text
+        course.teacher = TeacherGetter.get(1)
     }
 
     fun back(event: ActionEvent) {
         Window.getWindow().changeToView("courses_view")
+    }
+
+    private fun showMessage() {
+        val alert = Alert(Alert.AlertType.WARNING)
+        alert.title = "Adevertencia"
+        alert.contentText = "Faltan datos en el formulario"
+        alert.showAndWait()
     }
 }
